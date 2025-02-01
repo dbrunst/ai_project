04f3d799c82c14031f0bce2dd61600ab14e9808f
Dina Brunstein <dbrunst7@gmail.com>
2025-02-01 16:56:54 -0500

    open ai and vertex ai author parse init commit

---
+++ author_list.py
@@ -0,0 +1,60 @@
+from openai import OpenAI
+import json
+
+def generate():
+  client = OpenAI()
+  json_content = ''
+
+  with open("../raw_data.json", "r") as file:
+    json_content = json.load(file)
+
+  response = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+      {
+        "role": "system",
+        "content": [
+          {
+            "type": "text",
+            "text": "Given a hash of ids and text, return a json object with the id and an array of only people's names without prefixes but including capitalized suffixes"
+          }
+        ]
+      },
+      {
+        "role": "user",
+        "content": [
+          {
+            "type": "text",
+            "text": json.dumps(json_content)
+          }
+        ]
+      },
+      {
+        "role": "assistant",
+        "content": [
+          {
+            "type": "text",
+            "text": """
+              To extract only the people's names from the given text, we should look for names typically formatted with a first and last name, separated by a space or comma.
+              The input hash contains some examples of such names that can be extracted:
+                \n\n```json\n{\n  \"1\": [\"Zeke Miller III\", \"Aamer Madhani\", \"Daniella Genovese\"],\n  \"2\": [],\n  \"3\": [\"john smith\"]\n}\n```\n\n
+              This output correctly identifies and returns arrays of unique names from each string in the input hash.
+              It removes non-name text such as dates, publisher information, professional titles, and prefixes such as dr.
+              """
+          }
+        ]
+      }
+    ],
+    response_format={
+      "type": "json_object"
+    },
+    temperature=1,
+    max_completion_tokens=2048,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+  )
+
+  return json.loads(response.choices[0].message.content)
+
+print(json.dumps(generate(), indent=4))
---
+++ author_list_google.py
@@ -0,0 +1,65 @@
+import base64
+import json
+import vertexai
+from vertexai.generative_models import GenerativeModel, Part, SafetySetting
+
+
+
+generation_config = {
+    "max_output_tokens": 2048,
+    "temperature": 1,
+    "top_p": 1,
+}
+
+safety_settings = [
+    SafetySetting(
+        category=SafetySetting.HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold=SafetySetting.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE
+    ),
+    SafetySetting(
+        category=SafetySetting.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold=SafetySetting.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE
+    ),
+    SafetySetting(
+        category=SafetySetting.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold=SafetySetting.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE
+    ),
+    SafetySetting(
+        category=SafetySetting.HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold=SafetySetting.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE
+    ),
+]
+
+def generate():
+    vertexai.init(project="gemini-playground-441622", location="us-central1")
+    textsi_1 = """
+      Given a hash of ids and text, return a json object with the id and an array of only people's names without prefixes but including capitalized suffixes.
+      the reponse should just contain the json result wihtout any markdow
+      """
+
+    model = GenerativeModel(
+        "gemini-1.0-pro-002",
+        system_instruction=[textsi_1]
+    )
+
+
+    with open("../raw_data.json", "r") as file:
+      text1 = json.dumps(json.load(file))
+
+    responses = model.generate_content(
+        [text1],
+        generation_config=generation_config,
+        safety_settings=safety_settings,
+        stream=True,
+    )
+
+    raw = ''
+    for response in responses:
+        raw += response.text
+
+    raw = raw.replace("```json\n", "").replace("\n```", "")
+
+    raw = json.loads(raw)
+    print(json.dumps(raw, indent=4))
+
+generate()
